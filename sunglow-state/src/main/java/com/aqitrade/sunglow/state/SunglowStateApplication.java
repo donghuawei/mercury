e37c2417281126ece19a61eb5cfdc4bd23b2a6a6
Huawei <huaweirizhao@163.com>
2018-12-20 13:48:45 +0800

    Add new module sunglow-state

---
+++ sunglow-state/src/main/java/com/aqitrade/sunglow/state/SunglowStateApplication.java
@@ -0,0 +1,14 @@
+package com.aqitrade.sunglow.state;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunglowStateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunglowStateApplication.class, args);
+    }
+
+}
+
---
+++ sunglow-state/src/test/java/com/aqitrade/sunglow/state/SunglowStateApplicationTests.java
@@ -0,0 +1,17 @@
+package com.aqitrade.sunglow.state;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SunglowStateApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
